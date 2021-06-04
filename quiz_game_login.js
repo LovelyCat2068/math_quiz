e3fc25feab2201742749aa22d6f81e5b8ae92169
LovelyCat2068 <76818025+LovelyCat2068@users.noreply.github.com>
2021-06-04 19:19:35 +0530

    Update quiz_game_login.js

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -2,8 +2,9 @@ function addUser(){
     var player_1_name  = document.getElementById("player1_name").value;
     var player_2_name = document.getElementById("player2_name").value;
 
+    
+    window.location = "quiz_game_page.html";
+    
     window.localStorage.setItem(Player_1, player_1_name)
     window.localStorage.setItem(Player_2, player_2_name);
-
-    window.location = "quiz_game_page.html";
 }
