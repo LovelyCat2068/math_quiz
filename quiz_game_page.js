1ce7ae4754a3ce6343d794fdedc09ee92be0743b
LovelyCat2068 <76818025+LovelyCat2068@users.noreply.github.com>
2021-06-09 21:17:24 +0530

    Update quiz_game_page.js

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -1,7 +1,7 @@
 function send(){
-    number1 = document.getElementById("number1").value;
-    number2 = document.getElementById("number2").value;
-    actual_answer = parseInt(number1) * parseInt(number2);
+  var  number1 = document.getElementById("Number_1").value;
+   var number2 = document.getElementById("Number_2").value;
+   var actual_answer = parseInt(number1) * parseInt(number2);
     
     
 
@@ -10,10 +10,10 @@ input_box = "<br>Answer : <input type:text id='input_check_box'>";
 check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
 row = question_number +  input_box + check_button;
 document.getElementById("output").innerHTML = row;
-
-document.getElementById("number1").innerHTML = "";
-document.getElementById("number2").innerHTML = "";
     
   var player__1 =  window.localStorage.getItem("Player1");
     var player__2 =  window.localStorage.getItem("Player2");
+    
+    document.getElementById("player1_name").innerHTML = player__1;
+    document.getElementById("player2_name").innerHTML = player__2;
 }
