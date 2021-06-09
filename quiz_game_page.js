24cc8eecd530a26ae45450903bbe4148690ac5b6
LovelyCat2068 <76818025+LovelyCat2068@users.noreply.github.com>
2021-06-09 21:44:55 +0530

    Update quiz_game_page.js

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -1,9 +1,9 @@
 function send(){
-var number_1 = document.getElementById("Number_1").value;
-var number_2 = document.getElementById("Number_2").value;
+var number1 = document.getElementById("Number_1").value;
+var number2 = document.getElementById("Number_2").value;
 var actual_answer = parseInt(number1) * parseInt(number2);
 
-question_number = "<h4>" + number_1 + "x" + number_2 + "</h4>";
+question_number = "<h4>" + number1 + "x" + number2 + "</h4>";
 input_box = "<br>Answer : <input type:text id='input_check_box'>";
 check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
 row = question_number +  input_box + check_button;
