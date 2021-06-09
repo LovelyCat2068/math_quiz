afaca71b95cdeaff9043044aebe157bd93c4c357
LovelyCat2068 <76818025+LovelyCat2068@users.noreply.github.com>
2021-06-09 21:23:36 +0530

    Update quiz_game_page.js

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -1,1 +1,5 @@
+ var player__1 =  window.localStorage.getItem("Player1");
+    var player__2 =  window.localStorage.getItem("Player2");
 
+    document.getElementById("player1_name").innerHTML = player__1;
+    document.getElementById("player2_name").innerHTML = player__2;
