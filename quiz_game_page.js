b50a1b2c38590d4e44538dd34dfd49f059ba953b
LovelyCat2068 <76818025+LovelyCat2068@users.noreply.github.com>
2021-06-09 21:31:59 +0530

    Update quiz_game_page.js

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -1,5 +1,5 @@
- var player__1 =  localStorage.getItem("Player_1");
-    var player__2 =  localStorage.getItem("Player_2");
+ var player__1_ =  localStorage.getItem("Player_1");
+    var player__2_ =  localStorage.getItem("Player_2");
 
-    document.getElementById("player1_name").innerHTML = player__1;
-    document.getElementById("player2_name").innerHTML = player__2;
+    document.getElementById("player1_name").innerHTML = player__1_;
+    document.getElementById("player2_name").innerHTML = player__2_;
