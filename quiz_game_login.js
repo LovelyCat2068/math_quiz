965e937b0dbc1323ef9a639653b4205659087703
LovelyCat2068 <76818025+LovelyCat2068@users.noreply.github.com>
2021-06-10 21:04:15 +0530

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -7,3 +7,5 @@ function addUser(){
     
     window.location = "quiz_game_page.html";
 }
+
+
--- quiz_game_page.js
+++ quiz_game_page.js
@@ -1,17 +1,22 @@
+var playerOne_name = localStorage.getItem("Player_1");
+
+var playerTwo_name = localStorage.getItem("Player_2");
+
+var playerOne_score = 0;
+
+var playerTwo_score = 0;
+
+document.getElementById("player1_name").innerHTML = playerOne_name+": "+playerOne_score;
+document.getElementById("player2_name").innerHTML = playerTwo_name+": "+playerTwo_score;
+
 function send(){
-var number1 = document.getElementById("Number_1").value;
-var number2 = document.getElementById("Number_2").value;
-var actual_answer = parseInt(number1) * parseInt(number2);
+var number_1 = document.getElementById("number1").value;
+var number_2 = document.getElementById("number2").value;
+var actual_answer = parseInt(number_1) * parseInt(number_2);
 
-question_number = "<h4>" + number1 + "x" + number2 + "</h4>";
-input_box = "<br>Answer : <input type:text id='input_check_box'>";
-check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
-row = question_number +  input_box + check_button;
+var question_number = "<h4>" + number_1 + "x" + number_2 + "</h4>";
+var input_box = "<br>Answer : <input type:text id='input_check_box'>";
+var check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+var row = question_number +  input_box + check_button;
 document.getElementById("output").innerHTML = row;
- 
- var player__1 =  localStorage.getItem("Player_1");
- var player__2 =  localStorage.getItem("Player_2");
-
- document.getElementById("player1_name").innerHTML = player__1;
- document.getElementById("player2_name").innerHTML = player__2;
 }
